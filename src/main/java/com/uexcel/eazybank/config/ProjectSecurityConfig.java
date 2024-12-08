cda81c605f15f5234e39135e31a60c81ab2b832d
Uexcel2022 <excellenceudoka@gmail.com>
2024-12-08 10:29:31 +0100

    added secure login functionality

--- src/main/java/com/uexcel/eazybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/uexcel/eazybank/config/ProjectSecurityConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -28,13 +28,10 @@ public class ProjectSecurityConfig {
         http.httpBasic(withDefaults());
         return http.build();
     }
-
+    
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}!a4a4%").roles("USER").build();
-        UserDetails admin= User.withUsername("admin")
-                .password("{bcrypt}$2a$12$FFGa/C7P32tpG6hhTY3O4uNtZ57bqA9pWupnM5V0ZGBRrUf8y1bzS").roles("ADMIN").build();
-        return new  InMemoryUserDetailsManager(user,admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -47,5 +44,14 @@ public class ProjectSecurityConfig {
     public CompromisedPasswordChecker compromisedPasswordChecker() {
         return new HaveIBeenPwnedRestApiPasswordChecker();
     }
+
+    //    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.withUsername("user").password("{noop}!a4a4%").roles("USER").build();
+//        UserDetails admin= User.withUsername("admin")
+//                .password("{bcrypt}$2a$12$FFGa/C7P32tpG6hhTY3O4uNtZ57bqA9pWupnM5V0ZGBRrUf8y1bzS")
+//                .roles("ADMIN").build();
+//        return new  InMemoryUserDetailsManager(user,admin);
+//    }
 
 }
