0f3d5813ae7c4eb9c96a0ded3ba6a255aa01e1b6
Uexcel2022 <excellenceudoka@gmail.com>
2024-12-10 10:23:58 +0100

    improvement

--- src/main/java/com/uexcel/eazybank/config/EazyBankUsernamePwdAuthenticationProvider.java
+++ src/main/java/com/uexcel/eazybank/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -3,20 +3,16 @@ package com.uexcel.eazybank.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @AllArgsConstructor
-@Profile("prod")
-public class EazyBankUsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+@Profile("!prod")
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
     private final EazyBankUserDetailsService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
     /**
      * @param authentication the authentication request object.
      * @throws AuthenticationException - exception
@@ -26,11 +22,7 @@ public class EazyBankUsernamePasswordAuthenticationProvider implements Authentic
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        if (passwordEncoder.matches(password, userDetails.getPassword())) {
-            return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
-        } else {
-            throw new BadCredentialsException("Bad credentials.");
-        }
+        return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
     }
     /**
      * @param authentication - authentication
